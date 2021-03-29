2f107d86481ccea9ae2f56dbe9d060b6723bbb59
Pietro <marrazzo.pietroalfonso@gmail.com>
2021-03-29 14:17:06 +0200

    first push, burger 2 animation incomplete

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".burger1").click(function(){
+        $(".burger1").toggleClass("burger1-rotate");
+    })
+
+    $(".burger2").click(function(){
+      $(".burger2").toggleClass("open");
+    })
+
+    $(".burger3").click(function(){
+      $(".burger3").toggleClass("open");
+    })
+});
+
+console.log('hello there');
